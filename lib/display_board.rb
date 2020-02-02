751bd4a2aea3ff6d83207d4dc027041d591e763f
Maryan W. Berhe <maryanberhe@gmail.com>
2020-02-02 02:26:08 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,9 +5,9 @@ turn_1 = ["X"," "," "," "," "," "," "," "," "]
 def display_board(the_board)
   puts " #{the_board[0]} | #{the_board[1]} | #{the_board[2]} "
   puts "-----------"
-  puts " #{the_board[3]} | #{the_board[4]} | #{the_board[5]}  "
+  puts " #{the_board[3]} | #{the_board[4]} | #{the_board[5]} "
   puts "-----------"
-  puts " #{the_board[6]} | #{the_board[7]} | #{the_board[8]}  "
+  puts " #{the_board[6]} | #{the_board[7]} | #{the_board[8]} "
 end
 
 display_board(turn_1)
